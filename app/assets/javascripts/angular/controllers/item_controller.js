db97ca75c21c82b94e3880594a5c850e8820b3c6
Leo <leolopelofranco@gmail.com>
2016-12-03 23:09:05 +0800

    added item show

--- app/assets/javascripts/angular/controllers/item_controller.js
+++ app/assets/javascripts/angular/controllers/item_controller.js
@@ -1,8 +1,19 @@
 Bidly.controller('ItemController', ['$scope', '$location', 'DemoService', 'ngDialog',
   function($scope, $location, DemoService, ngDialog) {
 
+    $scope.selection = 'details';
+
+    $scope.tab = function(selected) {
+      $scope.selection = selected
+    }
 
     $scope.cards = DemoService.class();
+    $scope.item = {
+      "templateUrl": 'shared/_itemMainCard.html'
+    }
 
+    $scope.suggestions = {
+      "templateUrl": 'shared/_itemCard.html'
+    }
 
 }])
